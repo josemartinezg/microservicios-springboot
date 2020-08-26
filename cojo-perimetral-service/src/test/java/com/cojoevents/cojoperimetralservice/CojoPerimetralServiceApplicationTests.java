e26705dc18ec57eb13a3f4587ae0dbd98f0ca58b
OliverJPR <33287824+OliverJPR@users.noreply.github.com>
2020-08-25 23:06:35 -0400

    perimtral service

---
+++ cojo-perimetral-service/src/test/java/com/cojoevents/cojoperimetralservice/CojoPerimetralServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cojoevents.cojoperimetralservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CojoPerimetralServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
