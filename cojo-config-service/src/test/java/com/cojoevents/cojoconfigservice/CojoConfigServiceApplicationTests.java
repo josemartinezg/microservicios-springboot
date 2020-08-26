789d3c5b666e0c3dd24a7cce5ba0530e2036387e
OliverJPR <33287824+OliverJPR@users.noreply.github.com>
2020-08-25 22:20:06 -0400

    config server

---
+++ cojo-config-service/src/test/java/com/cojoevents/cojoconfigservice/CojoConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cojoevents.cojoconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CojoConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
